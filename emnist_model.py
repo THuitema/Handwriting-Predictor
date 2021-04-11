09c93d81857258f3f24055adfd14dcf0d984c1b0
THuitema <thuitema35@gmail.com>
2021-04-11 00:07:45 -0400

    EMNIST initial commit

---
+++ emnist_model.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+from keras import layers, models
+from keras.layers import Conv2D, Dense, Dropout, MaxPool2D, Flatten, BatchNormalization, Activation
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # hides tensorflow warnings
+
+CLASSES = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabdefghnqrt' # 47 classes
+
+def load_data():
+    # loading in data
+    train_data = pd.read_csv('data/emnist-balanced-train.csv')
+    test_data = pd.read_csv('data/emnist-balanced-test.csv')
+
+    y_train = train_data['45']
+    y_test = test_data['41']
+    x_train = train_data.drop('45',axis=1)
+    x_test = test_data.drop('41',axis=1)
+
+    return x_train, y_train, x_test, y_test
+
+def prep_data(x_train, y_train, x_test, y_test):
+    # normalizing data
+    x_train /= 255.0
+    x_test /= 255.0
+
+    x_train = x_train.values.reshape(-1, 28, 28, 1) # 28x28 pixel grayscale image
+    y_train = y_train.values
+    x_test = x_test.values.reshape(-1, 28, 28, 1)
+    y_test = y_test.values
+
+    return x_train, y_train, x_test, y_test
+
+
+x_train, y_train, x_test, y_test = load_data()
+x_train, y_train, x_test, y_test = prep_data(x_train, y_train, x_test, y_test)
+
+# model
+model = keras.Sequential()
+model.add(Conv2D(8, (5,5), padding='same',input_shape=(28, 28, 1), activation='relu'))
+
+# model.add(BatchNormalization())
+# model.add(Conv2D(28,(5,5), activation='relu'))
+# model.add(MaxPool2D(pool_size=(2,2)))
+# model.add(Dropout(0.25))
+#
+# model.add(Conv2D(32,(5,5),padding='same',input_shape=(28, 28, 1), activation='relu'))
+# model.add(BatchNormalization())
+# model.add(Conv2D(32, (5,5), activation='relu'))
+# model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu')) # 512
+
+model.add(Dropout(0.25))
+model.add(Dense(47, activation='softmax')) # 47 different outputs
+
+
+model.compile(
+    optimizer='adam',
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy']
+)
+
+model.fit(x_train, y_train, batch_size=128, epochs=4)
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+
+
+
---
+++ mnist_model.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers, models
+from keras.layers import Conv2D, Dense, Dropout, MaxPool2D, Flatten, BatchNormalization, Activation
+import matplotlib.pyplot as plt
+import ssl
+import os
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # hides tensorflow warnings
+
+# handles weird errors when loading dataset from keras
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+    
+def load_data():
+    mnist = keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+    # adding 1 at end to add dimension for just gray-scale values
+    x_train = x_train.reshape((x_train.shape[0], 28, 28, 1)) 
+    x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+
+    return x_train, y_train, x_test, y_test
+
+def prep_data(x_train, x_test):
+    x_train_normalized = x_train.astype('float32')
+    x_test_normalized = x_test.astype('float32')
+    
+    # Normalizing values between 0 and 1
+    x_train_normalized = keras.utils.normalize(x_train_normalized, axis=1)
+    x_test_normalized = keras.utils.normalize(x_test_normalized, axis=1)
+    return x_train_normalized, x_test_normalized
+
+x_train, y_train, x_test, y_test = load_data()
+x_train, x_test = prep_data(x_train, x_test)
+
+
+model = models.Sequential()
+model.add(Conv2D(28,(5,5),padding='same',input_shape=(28, 28, 1), activation='relu'))
+
+model.add(BatchNormalization())
+model.add(Conv2D(28,(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(32,(5,5),padding='same',input_shape=(28, 28, 1), activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32, (5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+
+model.add(Dropout(0.25))
+model.add(Dense(10, activation='softmax'))
+
+
+model.compile(
+    optimizer='adam',
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy']
+)
+
+model.fit(x_train, y_train, batch_size=128, epochs=8)
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+
+# saving model if val_acc > most accurate model
+high = .9890
+if val_acc > high:
+    model.save(f'models/acc-{round(val_acc, 4)}')
+    print('model saved')
+
+# loading model and showing predictions from x_test
+loaded = keras.models.load_model('models/acc-0.9942')
+predictions = loaded.predict([x_test])
+
+for count, i in enumerate(predictions):
+    print(i, y_test[count])
+    plt.imshow(x_test[count])
+    plt.title(f'Predicted: {np.argmax(i)}, Actual: {y_test[count]}')
+    plt.show()
+
